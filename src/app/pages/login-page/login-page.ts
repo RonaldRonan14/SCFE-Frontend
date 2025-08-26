f170a6ee512722fb10934bab2cd3c104059bef46
Ronald Brito <ronald.brito@tjsp.jus.br>
2025-08-26 18:54:43 -0300

    Autenticacao adicionada

--- src/app/pages/login-page/login-page.ts
+++ src/app/pages/login-page/login-page.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { Autenticacao, Token } from '../../models/token.model';
 import { AutenticacaoService } from '../../services/autenticacao.service';
+import { Router } from '@angular/router';
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-login-page',
@@ -17,7 +19,8 @@ export class LoginPage implements OnInit {
 
   constructor (
     private fb: FormBuilder,
-    private autenticacaoService: AutenticacaoService
+    private autenticacaoService: AutenticacaoService,
+    private router: Router,
   ){}
 
   ngOnInit(): void {
@@ -33,9 +36,12 @@ export class LoginPage implements OnInit {
 
     const autenticacao = this.autenticacaoForm.value as Autenticacao;
     this.autenticacaoService.ValidateCredentials(autenticacao).subscribe({
-      next: (dados) => this.token = dados,
-      error: (err) => {
-        alert(err)
+      next: (dados: Token) => {
+        this.autenticacaoService.setToken(dados);
+        this.router.navigate(['/dashboard']); 
+      },
+      error: (err: HttpErrorResponse) => {
+        alert(err.error);
       }
     })
   }
